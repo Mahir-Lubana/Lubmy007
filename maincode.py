0d62159b85c1d6e8ac1bed85367ca6961792dd98
Mahir Lubana <mahirlubana2004@gmail.com>
2023-11-16 16:06:35 +1030

    Added methods to Alchemist and Laboratory Class

--- maincode.py
+++ maincode.py
@@ -1,12 +1,15 @@
 '''
 File: maincode.py
-Description: A brief description of this Python module.
+Description: Main code file for assignment 2 
 Author: Mahir Lubana
 StudentID: 110408840
 EmailID: Lubmy007
 This is my own work as defined by the University's Academic Misconduct Policy.
 '''
-#Alchemist class 
+
+
+
+
 class Alchemist:
     def __init__(self, attack, strength, magic, defense, ranged, laboratory, necromancy):
         self.attack = attack
@@ -14,7 +17,7 @@ class Alchemist:
         self.magic = magic 
         self.defense = defense
         self.ranged = ranged
-        self.laboratory = Laboratory()
+        self.laboratory = laboratory
         self.necromancy = necromancy
         self.recipes = {
             "Super Attack": ["Irit", "Eye of Newt"],
@@ -30,6 +33,33 @@ class Alchemist:
             "Extreme Ranging":["Grenwall Spike", "Super Ranging"],
             "Extreme Necromancy":["Ground Miasama Rune", "Super Necromancy"] 
         }
+    
+
+    def getLaboratory(self):
+        return self.laboratory
+    
+    def getRecipes(self):
+        return self.recipes 
+    
+    def mixpotion(self, potion_name):
+        if potion_name in self.recipes:
+            ingredients = self.recipes[potion_name]
+            primary_ingredient, secondary_ingredient = ingredients
+            self.laboratory.mixPotion(potion_name, "some_type", "some_stat", primary_ingredient, secondary_ingredient)
+        else:
+            print(f'Error: Unknown potion recipe {potion_name} ')
+    
+    def drinkPotion(self, potion):
+        pass
+
+    def collectReagent(self, reagent, amount):
+        self.laboratory.addReagent(reagent, amount)
+    
+    def refineReagents(self):
+        self.laboratory.refineReagents()
+
+
+
 
 class Laboratory:
     def __init__(self):
@@ -37,6 +67,26 @@ class Laboratory:
         self.herbs = []
         self.catalysts = []
 
+    def addPotion(self, potion):
+        self.potions.append(potion)
+    
+    def addHerb(self, herb):
+        self.herbs.append(herb)
+
+    def addCatalyst(self, catalyst):
+        self.catalysts.append(catalyst)
+
+    def mixPotion(self, name, type, stat, primaryIngredient, secondaryIngredient):
+        
+        pass
+
+    def addReagent(self, reagent, amount):
+
+        pass
+
+    def refineReagent(self):
+
+        pass
 
 
 
--- testcode.py
+++ testcode.py
@@ -1,3 +1,11 @@
+'''
+File: testcode.py
+Description: Test code file for assignment 2 
+Author: Mahir Lubana
+StudentID: 110408840
+EmailID: Lubmy007
+This is my own work as defined by the University's Academic Misconduct Policy.
+'''
 
 
 
