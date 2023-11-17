88a0875177a8ee5730300534e1aa3d0eded3d7e1
Mahir Lubana <mahirlubana2004@gmail.com>
2023-11-17 21:11:24 +1030

    Test code is completed

--- testcode.py
+++ testcode.py
@@ -25,15 +25,36 @@ def catalyst():
 def potion():
     return Potion(name="Testing Potion", stat="Testing Stat", boost=5.0)
 
+def testing_add_potion(lab, potion):
+    lab.addPotion(potion)
+    assert lab.potions == [potion]
 
+def testing_add_herb(lab, herb):
+    lab.addHerb(herb)
+    assert lab.herbs ==[herb]
 
+def testing_add_catalyst(lab, catalyst):
+    lab.addCatalyst(catalyst)
+    assert lab.catalyst == [catalyst]
 
+def testing_mix_potion(lab, herb,catalyst):
+    lab.addHerb(herb)
+    lab.addCatalysts(catalyst)
+    lab.mixPotion(name= "Testing Potion", type= "Super", stat="Testing Stat", primaryIngredient= "Testing Herb", secondaryIngredient="Testing Catalyst")
+    assert len(lab.potions) == 1
+    assert lab.potions[0].getName() == "Testing Potion"
 
+def testing_add_reagent(lab,herb, catalyst):
+    lab.addReagent(herb, amount=1)
+    lab.addReagent(catalyst, amount=1)
+    assert lab.herbs == [herb]
+    assert lab.catalysts == [catalyst]
 
-
-
-
-
-
-
-
+def testing_refine_reagents(lab, herb, catalyst):
+    lab.addHerb(herb)
+    lab.addCatalyst(catalyst)
+    lab.refineReagents()
+    assert not lab.herbs[0].isGrimy()
+    assert lab.herbs[0].getPotency() == 5.0
+    assert catalyst.getQuality() == 2.6
+    
